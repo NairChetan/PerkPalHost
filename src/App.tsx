b0911e6e9f6fcac66dbe05b56bc41a9921c8f639
SarathS1 <saraths052001@gmail.com>
2024-08-08 22:47:18 +0530

    Initial clockin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Dummy from "./Pages/Dummy";
+import NewEntry from "./Components/Clockin/NewEntry/NewEntry";
+import UserLogin from "./Components/Clockin/UserLogin/UserLogin";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/NavBar/Navbar";
+import Clockin from './Pages/Clockin';
 
 function App() {
   return (
     <>
-      <Dummy />
+      <Navbar />
+      <Clockin />
+      <Footer/>
+
+
     </>
   );
 }
