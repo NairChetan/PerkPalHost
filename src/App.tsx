0e7b1393ca060f26930528fff4c22aee1dc52254
SarathS1 <saraths052001@gmail.com>
2024-08-17 10:07:20 +0530

    Commit 17/08

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Admin_Dashboard from "./Pages/Admin_Dashboard";
-import EmployeeDashboard from "./Pages/EmployeeDashboard";
-import LogIn_Page from "./Pages/LogIn_Page";
-import Clockin from "./Pages/Clockin";
-import PendingApproval from "./Pages/PendingApproval";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Admin_Dashboard from "./Pages/Admin_Dashboard";
+// import EmployeeDashboard from "./Pages/EmployeeDashboard";
+// import LogIn_Page from "./Pages/LogIn_Page";
+// import Clockin from "./Pages/Clockin";
+// import PendingApproval from "./Pages/PendingApproval";
+import Admin_Category from "./Pages/Admin_Category";
 
 const AppRoutes: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<LogIn_Page />} />
-      <Route path="/admin-dashboard" element={<Admin_Dashboard />} />
-      <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
-      <Route path="/clockin" element={<Clockin />} />
-      <Route path="/pending-approval" element={<PendingApproval />} />
-    </Routes>
-  </Router>
+  // <Router>
+  //   <Routes>
+  //     <Route path="/" element={<LogIn_Page />} />
+  //     <Route path="/admin-dashboard" element={<Admin_Dashboard />} />
+  //     <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+  //     <Route path="/clockin" element={<Clockin />} />
+  //     <Route path="/pending-approval" element={<PendingApproval />} />
+  //   </Routes>
+  // </Router>
+  <Admin_Category/>
 );
 
 export default AppRoutes;
