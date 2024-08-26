d81c98cd76cab0d5d1b7e40221c8f55be523f2c9
= <=>
2024-08-26 18:39:01 +0530

    Employeedashboard final updated

--- src/Components/EmployeeDashboard/Section2/GetPoints/NewEntry.tsx
+++ src/Components/EmployeeDashboard/Section2/GetPoints/NewEntry.tsx
@@ -46,7 +46,7 @@ const NewEntry = ({ addEntry }) => {
     onSubmit: async (values, { resetForm }) => {
       setLoading(true);
       setError(null);
-
+      const employeelocal = localStorage.getItem("employeeId");
       try {
         const entry = {
           categoryName: values.category,
@@ -54,8 +54,8 @@ const NewEntry = ({ addEntry }) => {
           description: values.description,
           duration: values.duration,
           proofUrl: proof ? proof.name : null,
-          createdBy: 3,  
-          employeeEmpId: 3, 
+          createdBy: employeelocal,  
+          employeeEmpId: employeelocal, 
         };
 
         await submitParticipation(entry);
