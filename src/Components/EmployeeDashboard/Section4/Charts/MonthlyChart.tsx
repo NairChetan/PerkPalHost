235b78596ba90dbfc2786b8592b45da5170b40ec
= <=>
2024-08-28 12:19:26 +0530

    updated code on 28-08-24

--- src/Components/EmployeeDashboard/Section4/Charts/MonthlyChart.tsx
+++ src/Components/EmployeeDashboard/Section4/Charts/MonthlyChart.tsx
@@ -16,18 +16,18 @@ const chartOptions = {
 
 const chartData = {
   labels: [
-    "January",
-    "February",
-    "March",
-    "April",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
     "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
   ],
   datasets: [
     {
