3d5a7a588801c04e67b8c91f2c6e5e28a5f6ddf2
NairChetan <sanjaynair8192@gmail.com>
2024-10-09 12:37:30 +0530

    removed proxy

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      // Proxy for API requests
-      '/api/v1/auth': {
-        target: 'https://172.16.4.89:8443', // The URL of your backend server
-        changeOrigin: true, // Needed for virtual hosted sites
-        secure: false, // Use this if your backend uses self-signed certs
-      },
-    },
   },
   preview: {
     port: 3000,
